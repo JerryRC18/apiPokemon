f42546dd45fb06e41fd36b7ab85b9075de38db81
Gerardo <jesusgrobles9@gmail.com>
2022-09-13 11:30:35 -0500

    Agregamos boton de mostrar todo y funciones para mostrar los pokemones y su imagen

--- js/app.js
+++ js/app.js
@@ -9,7 +9,8 @@ const btnFiltrar = document.getElementById('btnFiltrar')
 const input = document.getElementById('filtroPokemones')
 
 
-let pokemones = {}
+let pokemones = []
+let pokemonBuscado = {}
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchPokemones()
@@ -18,12 +19,60 @@ document.addEventListener('DOMContentLoaded', () => {
 btnFiltrar.addEventListener('click', () => {
     if(input.value > 0 && input.value){
         console.log('click en el filtro', input.value)
+        fetchPokemones(input.value)
     }
 })
 
-const fetchPokemones = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=100000")
+lista.addEventListener('click', e => {
+    if(e.target.classList.contains('btn-dark')){
+        const id = e.target.dataset.id
+        fetchPokemonBuscado(id)
+        console.log('id', id)
+    }
+     
+})
+
+const fetchPokemonBuscado = id => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then( async(res) => {
+        //console.log(await res.json())
+        pokemonBuscado = await res.json()
+        const pokemon = {
+            nombre: pokemonBuscado.name,
+            experiencia: pokemonBuscado.base_experience,
+            hp: pokemonBuscado.stats[0].base_stat,
+            ataque: pokemonBuscado.stats[1].base_stat,
+            defensa: pokemonBuscado.stats[2].base_stat,
+            especial: pokemonBuscado.stats[3].base_stat,
+            imgJuego: pokemonBuscado.sprites.front_default,
+            imgCvg: pokemonBuscado.sprites.other.dream_world.front_default,
+            img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonBuscado.id}.png`
+        }
+        pintarPokemon(pokemon)
+        //console.log('pokemon', pokemon)
+    })
+    .catch( error => {
+        console.log(error)
+    })
+}
+
+const pintarPokemon = pokemon => {
+    const pokemonBuscado = document.getElementById('pokemonBuscado')
+    const template = document.getElementById('card').content
+    const clone = template.cloneNode(true)
+    const fragment = document.createDocumentFragment()
+    pokemonBuscado.innerHTML = ''
+    clone.querySelector('.card-body-img').setAttribute('src', pokemon.imgCvg)
+
+    fragment.appendChild(clone)
+    pokemonBuscado.appendChild(fragment)
+}
+
+const fetchPokemones = (total) => {
+    const limit = total || 2000
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
         .then (async(res) => {
+            pokemones = []
             //console.log('res', await res.json())
             let data = await res.json()
             pokemones = await data.results
@@ -36,6 +85,7 @@ const fetchPokemones = () => {
 }
 
 const pintarPokemones = () => {
+    lista.innerHTML = ''
     pokemones.forEach((item, index) => {
         //console.log(item)
         infoPokemon.querySelectorAll('p')[0].textContent = item.name
